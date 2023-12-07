bc376d2fcd80a3b84619af88cac16810faeebf1c
czarnycap <czarnycap@gmail.com>
2023-12-07 12:57:21 +0100

    Update influxdb_netstats.sh

--- influxdb_netstats.sh
+++ influxdb_netstats.sh
@@ -1,3 +1,4 @@
+ 
 #!/bin/bash
 
 # InfluxDB parameters
@@ -5,16 +6,16 @@ INFLUXDB_URL="http://192.168.1.239:8086"
 DATABASE="EdgeX1"
 
 # Create the database (optional, uncomment if needed)
-curl -i -XPOST "${INFLUXDB_URL}/query" --data-urlencode "q=CREATE DATABASE ${DATABAS
+curl -i -XPOST "${INFLUXDB_URL}/query" --data-urlencode "q=CREATE DATABASE ${DATABASE}"
 
 # Get current bandwidth and hourly RX/TX rate using vnstat --oneline
 VNSTAT_ONELINE=$(vnstat --oneline)
 
 # Extract relevant data from vnstat oneline output
-CURRENT_RX=$(echo "${VNSTAT_ONELINE}" | awk -F';' '{print $10}' | sed 's/MiB//' | tr
-CURRENT_TX=$(echo "${VNSTAT_ONELINE}" | awk -F';' '{print $12}' | sed 's/kbit\/s//'
-HOURLY_RX=$(echo "${VNSTAT_ONELINE}" | awk -F';' '{print $5}' | sed 's/MiB//' | tr -
-HOURLY_TX=$(echo "${VNSTAT_ONELINE}" | awk -F';' '{print $14}' | sed 's/MiB\/s//' |
+CURRENT_RX=$(echo "${VNSTAT_ONELINE}" | awk -F';' '{print $10}' | sed 's/MiB//' | tr -d ' ')
+CURRENT_TX=$(echo "${VNSTAT_ONELINE}" | awk -F';' '{print $12}' | sed 's/kbit\/s//' | tr -d ' ')
+HOURLY_RX=$(echo "${VNSTAT_ONELINE}" | awk -F';' '{print $5}' | sed 's/MiB//' | tr -d ' ')
+HOURLY_TX=$(echo "${VNSTAT_ONELINE}" | awk -F';' '{print $14}' | sed 's/MiB\/s//' | tr -d ' ')
 
 # Data to be sent
 MEASUREMENT="network_stats"
@@ -25,7 +26,7 @@ FIELDS="current_rx=${CURRENT_RX},current_tx=${CURRENT_TX}"
 LINE_PROTOCOL="${MEASUREMENT},${TAGS} ${FIELDS} $(date +%s%N)"
 
 # Send data to InfluxDB using curl
-curl -i -XPOST "${INFLUXDB_URL}/write?db=${DATABASE}" --data-binary "${LINE_PROTOCOL
+curl -i -XPOST "${INFLUXDB_URL}/write?db=${DATABASE}" --data-binary "${LINE_PROTOCOL}"
 
 # Check for success (optional)
 if [ $? -eq 0 ]; then
